7399d43fcc4b376e9c013fa8ef5ba2d7958b4607
Paul Greyson <someone.named.paul@gmail.com>
2011-10-17 21:40:30 -0700

    add copyright

--- require.js
+++ require.js
@@ -1,12 +1,44 @@
+/***********************************************************************************************************************
+
+	Copyright (c) 2011 Paul Greyson
+
+	Permission is hereby granted, free of charge, to any person 
+	obtaining a copy of this software and associated documentation 
+	files (the "Software"), to deal in the Software without 
+	restriction, including without limitation the rights to use, 
+	copy, modify, merge, publish, distribute, sublicense, and/or 
+	sell copies of the Software, and to permit persons to whom the 
+	Software is furnished to do so, subject to the following 
+	conditions:
+
+	The above copyright notice and this permission notice shall be 
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+	OTHER DEALINGS IN THE SOFTWARE.
+
+***********************************************************************************************************************/
+
 /*global define:true require:true exports:true*/
 
 // OPTION: bring up to final spec when v0.5.x is stable
 	// see: http://nodejs.org/docs/v0.5.0/api/modules.html
 
+// browser/nodejs compatible define/require
+
+// LIMITATION: modules are referenced by base name so duplicate module names at different locations will conflict
+
+
 (function () {
+	
 	var modules = {};
 	var defs = {};
-	var oldRequire;
 	
 	function normalize(name) {
 		return name.split('/').slice(-1)[0].replace('.js', '');
@@ -20,10 +52,6 @@
 		};		
 
 		require = function (name) {
-			if (oldRequire) {
-				oldRequire(name);
-			}
-
 			var normalizedName = normalize(name);
 
 			if (modules && modules.hasOwnProperty(normalizedName)) {
@@ -45,8 +73,6 @@
 		define = function (name, exports, fn) {
 			fn(exports);
 		};
-	}
-	
-
+	}	
 }());
 
