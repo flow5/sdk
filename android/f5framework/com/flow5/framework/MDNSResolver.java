2a5a5412acdbc72d0a8033314dbc7e69ec791301
Paul Greyson <someone.named.paul@gmail.com>
2011-12-30 16:56:52 -0800

    Android: write hostname to xml and use for mDNS resolution on launch

---
+++ android/f5framework/com/flow5/framework/MDNSResolver.java
@@ -0,0 +1,72 @@
+package com.flow5.framework;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+import org.xbill.DNS.*;
+
+
+public class MDNSResolver {
+	
+//    android.net.wifi.WifiManager.MulticastLock lock;
+//    android.os.Handler handler = new android.os.Handler();
+
+	public MDNSResolver() {
+		
+//      android.net.wifi.WifiManager wifi = (android.net.wifi.WifiManager) getSystemService(android.content.Context.WIFI_SERVICE);
+//      lock = wifi.createMulticastLock("mylockthereturn");
+//      lock.setReferenceCounted(true);
+//      lock.acquire();
+
+//      lock.release();
+	}
+	
+	public String resolve(String localName) {
+		String result = null;
+		Record answer = null;
+		try {	
+			Name name = Name.fromString(localName);
+				
+			// setup the multicast socket for mDNS queries
+			MulticastSocket socket = new MulticastSocket(5353);
+			InetAddress group = InetAddress.getByName("224.0.0.251");
+			socket.joinGroup(group);
+			
+			// create a dns query
+			Record rec = Record.newRecord(name, Type.A, DClass.IN);
+			Message query = Message.newQuery(rec);
+			
+			// serialize and send
+			byte [] out = query.toWire(Message.MAXLENGTH);
+			DatagramPacket packet = new DatagramPacket(out, out.length, group, 5353);
+			socket.send(packet);
+			
+			// wait for an answer
+				while (answer == null) {
+					byte[] in = new byte[512];
+			 	    packet = new DatagramPacket(in, in.length);
+			 	    socket.receive(packet);        	    
+			 	                 		
+					Message response =  new Message(in);
+					Record records[] = response.getSectionArray(Section.ANSWER);
+					if (records != null) {
+						for (int i=0; i<records.length; ++i) {
+							if (records[i].getName().equals(name)) {
+								answer = records[i];
+							}
+						}
+					}   			
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}	
+		 
+		if (answer != null) {
+			 result = ((ARecord)answer).getAddress().getHostAddress();			
+		}
+		 
+		return result;
+	}
+	
+}
--- android/f5template/src/com/flow5/template/F5templateActivity.java
+++ android/f5template/src/com/flow5/template/F5templateActivity.java
@@ -1,6 +1,14 @@
 package com.flow5.template;
 
+import org.xmlpull.v1.XmlPullParser;
+
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Xml;
+
+import com.flow5.framework.MDNSResolver;
 import com.phonegap.*;
 import com.phonegap.api.LOG;
 
@@ -10,6 +18,43 @@ public class F5templateActivity extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
     	LOG.setLogLevel("VERBOSE");
         super.onCreate(savedInstanceState);
-        super.loadUrl("http://10.0.1.5:8008/generate?app=jitc&native=true&inline=true&debug=true");
+                
+        final F5templateActivity activity = this;
+        
+        new Thread(new Runnable() {
+			@Override
+			public void run() {
+				String devservhostname = "unknown.local";
+				
+				XmlPullParser xpp = getApplicationContext().getResources().getXml(R.xml.f5);				
+				try {
+					while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {						 
+				        if (xpp.getEventType() == XmlPullParser.START_TAG) {	
+				        	if (xpp.getName().equals("devservhost")) {
+				        		LOG.d("F5", xpp.getName());
+				        		devservhostname = xpp.getAttributeValue(null, "name");
+					        	break;
+				        	}
+				        }				 
+				        xpp.next();				 
+					}									
+				} catch (Exception e) {
+					LOG.d("F5", e.getMessage());
+				}
+				
+				MDNSResolver resolver = new MDNSResolver();               
+                final String address = resolver.resolve(devservhostname);
+                
+                activity.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						String url = "http://" + address + ":8008/generate?app=jitc&native=true&inline=true&debug=true";
+						LOG.d("F5", url);
+		                activity.loadUrl(url);					
+					}
+                	
+                });				
+			}        	
+        }).start();
     }
 }
