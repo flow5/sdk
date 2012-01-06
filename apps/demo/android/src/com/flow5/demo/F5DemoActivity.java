98b0162f16e9b6717877da3ca311eb4c3f1ad09f
Paul Greyson <someone.named.paul@gmail.com>
2012-01-05 22:03:26 -0800

    basic setup for demo app

---
+++ apps/demo/android/src/com/flow5/demo/F5DemoActivity.java
@@ -0,0 +1,12 @@
+package com.flow5.demo;
+
+import android.os.Bundle;
+
+import com.flow5.framework.*;
+import com.flow5.demo.R;
+
+public class F5DemoActivity extends F5Activity {
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState, R.xml.f5config);
+    }		
+}
