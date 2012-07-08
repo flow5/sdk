d42a8123fe44b9795a8952949a4b564ba3939836
Paul Greyson <paul@flow5.com>
2012-07-08 13:46:42 -0700

    workaround for weird iOS event issue with tap events. also fixes the problem of not always getting touch feedback on quick touches

--- www/lib/events.js
+++ www/lib/events.js
@@ -180,7 +180,7 @@
 	F5.maxClickDistance = 10;
 	F5.maxClickTime = 1000;
 
-	F5.addTapListener = function (el, cb, pressTime) {		
+	F5.addTapListener = function (el, cb, pressTime) {	
 		addEventListener(el, startEventName(), function (startEvent) {	
 			var cancel = false;
 
@@ -207,15 +207,21 @@
 				var clickTime = stopEvent.timeStamp - startEvent.timeStamp;
 				var clickMove = F5.eventDistance(startLoc, stopLoc);
 
-				if (pressTime) {
-					if (clickTime >= pressTime && clickMove <= F5.maxClickDistance) {
-						F5.callback(cb, stopEvent);
-					}										
-				} else {
-					if (clickTime <= F5.maxClickTime && clickMove <= F5.maxClickDistance && !cancel) {
-						F5.callback(cb, stopEvent);
-					}					
-				}				
+				// NOTE: not sure why the set timeout is needed. but without, sometimes
+				// the event gets delayed. possibly an interaction with animation
+				// in any case, the small delay guarantees that touch feedback gets a chance
+				// to display
+				setTimeout(function () {
+					if (pressTime) {
+						if (clickTime >= pressTime && clickMove <= F5.maxClickDistance) {
+							F5.callback(cb, stopEvent);
+						}										
+					} else {
+						if (clickTime <= F5.maxClickTime && clickMove <= F5.maxClickDistance && !cancel) {
+							F5.callback(cb, stopEvent);
+						}					
+					}									
+				}, 30);
 
 				F5.addTapListener(el, cb, pressTime);
 
