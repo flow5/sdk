fde0b37d3e22f9ec1b6a7ae8e31bec8a1dd3cde6
Paul Greyson <paul@flow5.com>
2013-06-04 17:16:03 -0700

    simplifed web activity spinner

--- www/packages/widgets/js/activity_native.js
+++ www/packages/widgets/js/activity_native.js
@@ -2,25 +2,25 @@
 
 	Copyright (c) 2012 Paul Greyson
 
-	Permission is hereby granted, free of charge, to any person 
-	obtaining a copy of this software and associated documentation 
-	files (the "Software"), to deal in the Software without 
-	restriction, including without limitation the rights to use, 
-	copy, modify, merge, publish, distribute, sublicense, and/or 
-	sell copies of the Software, and to permit persons to whom the 
-	Software is furnished to do so, subject to the following 
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
 	conditions:
 
-	The above copyright notice and this permission notice shall be 
+	The above copyright notice and this permission notice shall be
 	included in all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 	OTHER DEALINGS IN THE SOFTWARE.
 
 ***********************************************************************************************************************/
@@ -28,27 +28,27 @@
 
 
 F5.registerModule(function (F5) {
-	
+
 	function Activity() {
-		
-		this.initialize = function () {			
-			F5.addClass(this.el, 'f5activityspritecontainer');
+
+		this.initialize = function () {
+			F5.addClass(this.el, 'f5activityroot');
 		};
-						
+
 		this.start = function (el) {
-			el.appendChild(this.el);			
-					
+			el.appendChild(this.el);
+
 			var pos = F5.elementAbsolutePosition(el);
 			var bounds = {top: pos.y, left: pos.x, width: el.offsetWidth, height: el.offsetHeight};
-			
+
 			PhoneGap.exec(
 				function (result) { // success
 					console.log(result);
 			}, function (result) { // failure
 					console.log(result);
-			}, "com.flow5.activity", "start", [bounds]);	
+			}, "com.flow5.activity", "start", [bounds]);
 		};
-		
+
 		this.stop = function (el) {
 			// make sure that the activity is actually contained by this element
 			var search = this.el;
@@ -60,17 +60,17 @@ F5.registerModule(function (F5) {
 			}
 
 			if (search) {
-				this.el.parentElement.removeChild(this.el);			
+				this.el.parentElement.removeChild(this.el);
 			}
-						
+
 			PhoneGap.exec(
 				function (result) { // success
 					console.log(result);
 			}, function (result) { // failure
 					console.log(result);
-			}, "com.flow5.activity", "stop", []);	
+			}, "com.flow5.activity", "stop", []);
 		};
 	}
-	
-	F5.Prototypes.Widgets.Activity = new Activity();	
+
+	F5.Prototypes.Widgets.Activity = new Activity();
 });
--- www/packages/widgets/js/activity_web.js
+++ www/packages/widgets/js/activity_web.js
@@ -2,25 +2,25 @@
 
 	Copyright (c) 2011 Paul Greyson
 
-	Permission is hereby granted, free of charge, to any person 
-	obtaining a copy of this software and associated documentation 
-	files (the "Software"), to deal in the Software without 
-	restriction, including without limitation the rights to use, 
-	copy, modify, merge, publish, distribute, sublicense, and/or 
-	sell copies of the Software, and to permit persons to whom the 
-	Software is furnished to do so, subject to the following 
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
 	conditions:
 
-	The above copyright notice and this permission notice shall be 
+	The above copyright notice and this permission notice shall be
 	included in all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 	OTHER DEALINGS IN THE SOFTWARE.
 
 ***********************************************************************************************************************/
@@ -28,29 +28,21 @@
 
 
 F5.registerModule(function (F5) {
-	
+
 	function Activity() {
-		
-		this.initialize = function () {			
-			F5.addClass(this.el, 'f5activityspriteroot');
-			
-			var container = document.createElement('div');
-			F5.addClass(container, 'f5activityspritecontainer');
-			this.el.appendChild(container);
 
-			var mask = document.createElement('div');
-			F5.addClass(mask, 'f5activityspritemask');
-			container.appendChild(mask);
+		this.initialize = function () {
+			F5.addClass(this.el, 'f5activityroot');
 
-			var sprite = document.createElement('div');
-			F5.addClass(sprite, 'f5activitysprite');
-			mask.appendChild(sprite);							
+			var div = document.createElement('div');
+			F5.addClass(div, 'f5activity');
+			this.el.appendChild(div);
 		};
-		
-		this.start = function (el) {		
-			el.appendChild(this.el);			
+
+		this.start = function (el) {
+			el.appendChild(this.el);
 		};
-		
+
 		this.stop = function (el) {
 			// make sure that the activity is actually contained by this element
 			var search = this.el;
@@ -62,11 +54,11 @@ F5.registerModule(function (F5) {
 			}
 
 			if (search) {
-				this.el.parentElement.removeChild(this.el);			
-			}			
+				this.el.parentElement.removeChild(this.el);
+			}
 		};
 	}
-	
+
 	F5.Prototypes.Widgets.Activity = new Activity();
-	
+
 });
